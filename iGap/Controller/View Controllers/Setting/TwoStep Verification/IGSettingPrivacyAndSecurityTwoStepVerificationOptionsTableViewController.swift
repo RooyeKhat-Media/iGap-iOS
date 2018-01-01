d8816b3be3428c557437bc5101a91f0654db15e0
Saeed Mozaffari iOS <mozafari@rooyekhat.co>
2018-01-01 10:59:04 +0330

    Improvement - TwoStep Remove Password

--- iGap/Controller/View Controllers/Setting/TwoStep Verification/IGSettingPrivacyAndSecurityTwoStepVerificationOptionsTableViewController.swift
+++ iGap/Controller/View Controllers/Setting/TwoStep Verification/IGSettingPrivacyAndSecurityTwoStepVerificationOptionsTableViewController.swift
@@ -19,6 +19,7 @@ class IGSettingPrivacyAndSecurityTwoStepVerificationOptionsTableViewController: 
     @IBOutlet weak var unverifiedEmailAddressLabel: IGLabel!
     
     var twoStepVerification: IGTwoStepVerification?
+    var password: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,7 @@ class IGSettingPrivacyAndSecurityTwoStepVerificationOptionsTableViewController: 
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
-            //remove password
+            self.unsetPassword()
         } else if indexPath.row == 1 {
             self.performSegue(withIdentifier: "showChangePassword", sender: self)
         } else if indexPath.row == 2 {
@@ -48,5 +49,35 @@ class IGSettingPrivacyAndSecurityTwoStepVerificationOptionsTableViewController: 
 
     @IBAction func didTapOnResentVerifyCodeButton(_ sender: UIButton) {
         
+    }
+    
+    func unsetPassword(){
+        let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+        hud.mode = .indeterminate
+        IGUserTwoStepVerificationUnsetPasswordRequest.Generator.generate(password: self.password!).success({ (protoResponse) in
+            DispatchQueue.main.async {
+                hud.hide(animated: true)
+                switch protoResponse {
+                case let unsetPassword as IGPUserTwoStepVerificationUnsetPasswordResponse :
+                    IGUserTwoStepVerificationUnsetPasswordRequest.Handler.interpret(response: unsetPassword)
+                    self.navigationController?.popViewController(animated: true)
+                default:
+                    break
+                }
+            }
+        }).error ({ (errorCode, waitTime) in
+            DispatchQueue.main.async {
+                switch errorCode {
+                case .timeout:
+                    let alert = UIAlertController(title: "Timeout", message: "Please try again later", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okAction)
+                    self.present(alert, animated: true, completion: nil)
+                default:
+                    break
+                }
+                hud.hide(animated: true)
+            }
+        }).send()
     }
 }
